4d11946603a33814432e3976bc56e211220d124a
Sean Carter <sean.carter@sitecore.com>
2025-06-25 15:47:42 +0100

    put in next/third-parties/google library

--- headapps/nextjs-starter/src/pages/_app.tsx
+++ headapps/nextjs-starter/src/pages/_app.tsx
@@ -2,14 +2,16 @@ import type { AppProps } from 'next/app';
 import { I18nProvider } from 'next-localization';
 import { SitecorePageProps } from 'lib/page-props';
 import Bootstrap from 'src/Bootstrap';
-import CustomScripts from 'components/CustomScript';
-import GTMNoscript from 'components/GTMNoscript';
+//import CustomScripts from 'components/CustomScript';
+//import GTMNoscript from 'components/GTMNoscript';
 import useDataLayer from '../hooks/useDataLayer';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 import 'assets/main.scss';
 
 function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element {
   const { dictionary, ...rest } = pageProps;
+  //.const tagId = process.env.NEXT_PUBLIC_GTM_ID;
   useDataLayer(pageProps);
   return (
     <>
@@ -18,11 +20,12 @@ function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element
         // Use the next-localization (w/ rosetta) library to provide our translation dictionary to the app.
         // Note Next.js does not (currently) provide anything for translation, only i18n routing.
         // If your app is not multilingual, next-localization and references to it can be removed.
+        <CustomScripts locale={pageProps.locale} />
+        <GTMNoscript />
       */}
       <I18nProvider lngDict={dictionary} locale={pageProps.locale}>
         <>
-        <CustomScripts locale={pageProps.locale} />
-        <GTMNoscript />
+        <GoogleAnalytics gaId="G-KZ67Y8685K" />
         <Component {...rest} />
         </>
       </I18nProvider>
