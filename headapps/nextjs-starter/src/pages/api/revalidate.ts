8062177af1139b8adbadcec97b19869df2191518
Sean Carter <sean.carter@sitecore.com>
2025-07-03 19:00:52 +0100

    no message

--- headapps/nextjs-starter/src/pages/api/revalidate.ts
+++ headapps/nextjs-starter/src/pages/api/revalidate.ts
@@ -1,30 +1,22 @@
 /* eslint-disable */
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
     console.log(`[revalidation]: Start. Request: ${JSON.stringify(req.body)}`);
 
     if (req.query.secret !== process.env.ISR_REVALIDATE_TOKEN) {
       console.log(`[revalidate]: invalid token passed in "secret" param "${req.query.secret}"`)
       return res.status(401).json({ message: 'Invalid token' })
     }
+
+    const pathToRevalidate = req.query.path;
+    console.log('path to revalidate : ' + pathToRevalidate);
   
     try {
-      var layoutUpdates = req.body.updates.filter((update: any) =>  update.entity_definition == "LayoutData");
-      console.log(`[revalidate] Discovered ${layoutUpdates.length} layout updates.`);
+      const response = await res.revalidate(pathToRevalidate);
+      console.log(response);
+      return res.json({revalidated: true})
 
-      for await (const update of layoutUpdates) {
-          let edgePpath = update.identifier;
-          let actualPath = `/_site_${edgePpath.slice(0,-1)}`;//"/" + edgePpath.substring(edgePpath.indexOf('/') + 1);
-
-          console.log(`[revalidate]: Revalidating edge path "${edgePpath}" as "${actualPath}"`);
-         
-          // This probably needs its own error handling, to allow the pages to revalidate.
-          await res.revalidate(actualPath);
-      }
-      console.log("[revalidate]: Done!");
-      return res.json({ revalidated: true })
     } catch (err) {
-      console.log(`[revalidation]: Error during revalidation. Error: ${err}`);
-      return res.status(500).send('Error revalidating')
+      return res.status(500).send(`Error revalidating ${err}`)
     }
   }
